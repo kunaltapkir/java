6b6063fff0fe8165b07afac9708127ab39a87bdd
Kunal Tapkir <kunal.tapkir@synerzip.com>
2019-10-14 13:49:32 +0530

    test file added

---
+++ primefactortest.java
@@ -0,0 +1,51 @@
+package primefactor.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class primefactortest {
+
+	private List<Integer> list(Integer... integers) {
+		return Arrays.asList(integers);
+	}
+
+	@Test
+	public void testOne() {
+		assertEquals(list(), Primefactor.generate(1));
+	}
+
+	@Test
+	public void testTwo() {
+		assertEquals(list(2), Primefactor.generate(2));
+	}
+
+	@Test
+	public void testThree() {
+		assertEquals(list(3), Primefactor.generate(3));
+	}
+
+	@Test
+	public void testFour() {
+		assertEquals(list(2, 2), Primefactor.generate(4));
+	}
+
+	@Test
+	public void testSix() {
+		assertEquals(list(2, 3), Primefactor.generate(6));
+	}
+
+	@Test
+	public void testEight() {
+		assertEquals(list(2, 2, 2), Primefactor.generate(8));
+	}
+
+	@Test
+	public void testNine() {
+		assertEquals(list(3, 3), Primefactor.generate(9));
+	}
+
+}
