14b4c62f9ed648d46100c5853f39a20a1a2301cb
Kunal Tapkir <kunal.tapkir@synerzip.com>
2019-10-14 13:51:00 +0530

    changed test file

--- primefactortest.java
+++ primefactortest.java
@@ -1,11 +1,3 @@
-package primefactor.test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
 
 class primefactortest {
 
